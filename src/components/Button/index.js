7642706e821944e5c402a04ed5981d876aee21fc
Carolinagriffo <anacarolinagriffo@gmail.com>
2020-07-30 18:04:09 -0300

    Alterações no estilo e atualização para o heroku

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Button = styled.button`
     color: #ec90cb;
     border: 1px solid #ec90cb;
+    background: black;
     box-sizing: border-box;
     cursor: pointer;
     padding: 16px 24px;
